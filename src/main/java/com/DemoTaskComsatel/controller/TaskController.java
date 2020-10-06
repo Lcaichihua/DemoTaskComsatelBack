2d4a8509636e002e89c16c1d0e4020d5100b0857
Larry Caichihua Vergara <lcaichihua@hotmail.com>
2020-10-05 22:59:33 -0500

    cros origin set endpoints

--- src/main/java/com/DemoTaskComsatel/controller/TaskController.java
+++ src/main/java/com/DemoTaskComsatel/controller/TaskController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.DemoTaskComsatel.exception.ModeloNotFoundException;
@@ -23,6 +24,8 @@ import com.DemoTaskComsatel.model.Task;
 import com.DemoTaskComsatel.service.TaskService;
 
 @RestController
+@CrossOrigin(origins = "*", methods = { RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT,
+		RequestMethod.DELETE })
 @RequestMapping("/task")
 public class TaskController {
 	
